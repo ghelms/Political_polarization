dd78cd3c3768af117e153bb714da2cf649274ba6
mortgad <mortengade7400@hotmail.com>
2021-09-28 20:47:28 +0200

    Added some csv files

--- Folketinget-Scraping/04_plan_segment.sh
+++ Folketinget-Scraping/04_plan_segment.sh
@@ -2,4 +2,4 @@
 
 echo "[ ] Starting text segmenting"
 # cpu bound, so use only NCORES number of threads
-/usr/bin/find data/txt/*.txt | parallel --gnu -j 4 -X "python3 04_segment.py"
+find data/txt/*.txt | parallel --gnu -j 4 -X "python3 04_segment.py"
